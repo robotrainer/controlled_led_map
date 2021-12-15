8cee864fda11cf2924f7016566964a2c2aebf8bc
robotrainer <asbu26@gmail.com>
2021-12-15 22:09:26 +0700

    logic has been changed

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,39 @@
 let socket = io();
-let button_on = 1;
+let button_on_ig = 1;
+let button_on_yay = 2;
 
 const button = document.getElementById("button");
-const district = document.querySelectorAll(".district");
+const district = document.querySelectorAll(".text__district");
 
 district.forEach(d => {
   d.addEventListener("click", function () {
-    const active = Object.values(this.classList).includes("active");
+    // const active = Object.values(this.classList).includes("active");
     const districtName = this.getAttribute("data-title");
 
-    if (active) {
-      this.classList.remove("active");
-    } else {
-      district.forEach(d => {
-        d.classList.remove("active");
-      });
-      this.classList.add("active");
-    }
+    // if (active) {
+    //   this.classList.remove("active");
+    // } else {
+    //   district.forEach(d => {
+    //     d.classList.remove("active");
+    //   });
+    //   this.classList.add("active");
+    // }
 
     if (districtName == "Ижморский") {
-      socket.emit("click district", button_on);
-      if (button_on) {
-        button_on = 0;
+      socket.emit("click district", button_on_ig);
+      if (button_on_ig == 1) {
+        button_on_ig = 11;
       } else {
-        button_on = 1;
+        button_on_ig = 1;
+      }
+    }
+
+    if (districtName == "Яйский") {
+      socket.emit("click district", button_on_yay);
+      if (button_on_yay == 2) {
+        button_on_yay = 22;
+      } else {
+        button_on_yay = 2;
       }
     }
 
